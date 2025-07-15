8f3bd42a2dc41082f213c449c1c24be8887ce788
ReGitnald <rverrier01@manhattan.edu>
2025-07-14 21:39:56 -0400

    Finalized program

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import re
+import sys
+from stats import get_num_words
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 def main():
-    book_path = "books/frankenstein.txt"
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     print(f"{num_words} words found in the document")
 
-
-def get_num_words(text):
-    words = text.split()
-    return len(words)
 
 
 def get_book_text(path):
@@ -25,18 +26,30 @@ def get_character_count(text):
     return char_dict
 
 def book_report(book_path, text):
-    headline = f"Report of{book_path}"
-    report = headline + f"\n{get_num_words(text)} words found in the document\n"
+    headline = f"============ BOOKBOT ============]\nAnalyzing book found at {book_path}...\n----------- Word Count ----------"
+    report = headline + f"\nFound {get_num_words(text)} total words\n--------- Character Count -------"
     cdict = get_character_count(text)
     sortc = sorted(cdict,reverse=True, key=cdict.__getitem__)
     # print(sortc)
     for char in sortc:
         if char.isalpha():
-            line = f"\nThe {char} character was found {cdict[char]} times"
-            report = report+ line
+            line = f"\n{char}: {cdict[char]}"
+            report = report + line
     return report
 
+# def book_report(book_path, text):
+#     headline = f"Report of{book_path}"
+#     report = headline + f"\n{get_num_words(text)} words found in the document\n"
+#     cdict = get_character_count(text)
+#     sortc = sorted(cdict,reverse=True, key=cdict.__getitem__)
+#     # print(sortc)
+#     for char in sortc:
+#         if char.isalpha():
+#             line = f"\nThe {char} character was found {cdict[char]} times"
+#             report = report+ line
+#     return report
+
 # main()
-book_path = "books/frankenstein.txt"
+book_path = sys.argv[1]
 text = get_book_text(book_path)
 print(book_report(book_path, text))
